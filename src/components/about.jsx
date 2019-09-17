ffe196c27c3439671e1b9ad4da64ea2acfee4d1a
Michael Meng <michael_meng@v1040-wn-rt-b-11-237.campus-dynamic.uwaterloo.ca>
2019-09-17 12:57:34 -0400

    initial commit

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,70 +1,102 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class About extends Component {
   render() {
     return (
       <div>
         <section className="colorlib-about" data-section="about">
-        <div className="colorlib-narrow-content">
+          <div className="colorlib-narrow-content">
             <div className="row">
-            <div className="col-md-12">
-                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
-                <div className="col-md-12">
+              <div className="col-md-12">
+                <div
+                  className="row row-bottom-padded-sm animate-box"
+                  data-animate-effect="fadeInLeft"
+                >
+                  <div className="col-md-12">
                     <div className="about-desc">
-                    <span className="heading-meta">About Us</span>
-                    <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a computer grad student from DAIICT-Gandhinagar with ICT as my major.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
-                    <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
+                      <span className="heading-meta">About Me</span>
+                      <h2 className="colorlib-heading">Who Am I?</h2>
+                      <p>
+                        I am currently pursuing a degree in{" "}
+                        <strong>
+                          Computer Engineering at the University of Waterloo
+                        </strong>
+                        . Some of my interests involve Software Development, Web
+                        and Mobile Development, Robotics, Artificial
+                        Intelligence, Embedded Systems, Internet of Things and
+                        more :))
+                      </p>
+                      <p>
+                        I am always looking for things to do and ways to
+                        contribute and learn. If you have any contract or
+                        volunteer software development or robotics/mechanical
+                        design oppurtunities I would love to get in contact with
+                        you!
+                      </p>
                     </div>
+                  </div>
                 </div>
-                </div>
+              </div>
             </div>
-            </div>
-        </div>
+          </div>
         </section>
         <section className="colorlib-about">
-        <div className="colorlib-narrow-content">
+          <div className="colorlib-narrow-content">
             <div className="row">
-            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
                 <span className="heading-meta">What I do?</span>
-                <h2 className="colorlib-heading">Here are some of my expertise</h2>
-            </div>
+                <h2 className="colorlib-heading">
+                  Here are some of my expertise
+                </h2>
+              </div>
             </div>
             <div className="row row-pt-md">
-            <div className="col-md-4 text-center animate-box">
+              <div className="col-md-4 text-center animate-box">
                 <div className="services color-1">
-                <span className="icon">
-                    <i className="icon-bulb" />
-                </span>
-                <div className="desc">
-                    <h3>Web Development </h3>
-                    <p>I have experience building websites and chrome extentions using JavaScript,React,HTML,CSS</p>
+                  <span className="icon">
+                    <i className="icon-code" />
+                  </span>
+                  <div className="desc">
+                    <h3>Software Languages</h3>
+                    <p>
+                      I have experience with Python, C++, C, Javascript, Java,
+                      SQL, HTML, CSS, MATLAB, RISC-V
+                    </p>
+                  </div>
                 </div>
-                </div>
-            </div>
-            <div className="col-md-4 text-center animate-box">
+              </div>
+              <div className="col-md-4 text-center animate-box">
                 <div className="services color-3">
-                <span className="icon">
-                    <i className="icon-phone3" />
-                </span>
-                <div className="desc">
-                    <h3>Data Structures & Algorithms</h3>
-                    <p>As coming from the CS background, I have good grasp over fundamental concepts of DSA</p>
+                  <span className="icon">
+                    <i className="icon-download" />
+                  </span>
+                  <div className="desc">
+                    <h3>Frameworks</h3>
+                    <p>
+                      I have worked with Bootstrap, Angular, Node.js, MongoDB,
+                      Mongoose, React Native, Jest, Django, ROS
+                    </p>
+                  </div>
                 </div>
-                </div>
-            </div>
-            <div className="col-md-4 text-center animate-box">
+              </div>
+              <div className="col-md-4 text-center animate-box">
                 <div className="services color-5">
-                <span className="icon">
+                  <span className="icon">
                     <i className="icon-data" />
-                </span>
-                <div className="desc">
-                    <h3>Dev Ops</h3>
-                    <p>I am pursuing my internship with DevOps team at Juniper and working with tools like Jenkins, Docker, K8s</p>
+                  </span>
+                  <div className="desc">
+                    <h3>Technologies</h3>
+                    <p>
+                      I am familiar with Linux, Heroku, Firebase, Google Cloud
+                      Platform, Git, SVN, Jenkins, Jira, VirtualBox, Bluetooth
+                    </p>
+                  </div>
                 </div>
-                </div>
-            </div>
-            {/*
+              </div>
+              {/*
             <div className="col-md-4 text-center animate-box">
                 <div className="services color-2">
                 <span className="icon">
@@ -100,9 +132,9 @@ export default class About extends Component {
             </div>
             */}
             </div>
-        </div>
+          </div>
         </section>
       </div>
-    )
+    );
   }
 }
--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Timeline extends Component {
   render() {
@@ -7,7 +7,10 @@ export default class Timeline extends Component {
         <section className="colorlib-experience" data-section="timeline">
           <div className="colorlib-narrow-content">
             <div className="row">
-              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
                 <span className="heading-meta">highlights</span>
                 <h2 className="colorlib-heading animate-box">Timeline</h2>
               </div>
@@ -15,43 +18,263 @@ export default class Timeline extends Component {
             <div className="row">
               <div className="col-md-12">
                 <div className="timeline-centered">
-                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
-                    <div className="timeline-entry-inner">
-                      <div className="timeline-icon color-3">
-                        <i className="icon-pen2" />
-                      </div>
-                      <div className="timeline-label">
-                        <h2>Internship at Juniper <span>2019-present</span></h2>
-                        <p>I recently have joined the Juniper Networs as a Software Developer-Intern in the DevOps team. My major part of the work has been into the field of creating sustainable and flexible CI/CD (Continuous Integration & Continuous Development) pipelines which enables developers at Juniper to do rapid development. I am also exploring the role of container technology like Docker and container-orchrastation technologies like K8s, Swarm to create industry standard production environments.</p>
-                      </div>
-                    </div>
-                  </article>
-                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
                     <div className="timeline-entry-inner">
                       <div className="timeline-icon color-4">
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Undergraduation at DAIICT<span>2015-2019</span></h2>
-                        <p>I am pursuing my under-graduation studies with major in I.C.T.(Information & Communication Technology). I have taken courses like DSA, OOPs, Computer Networks, IOT, Web-Data Management over the years and have better understanding of these subjects.I have also been part of S.B.G.(Student Body Govenance) scince my first year of college.</p>
+                        <h2>
+                          Software Developer Intern <br></br>
+                          <span>Waterloo, Ontario </span>
+                          <br></br>
+                          <span>May. 2019 - Aug. 2019</span>
+                        </h2>
+                        <h2>Opentext Exstream Devops and Framework Team</h2>
+                        <p>
+                          •Worked in an agile software development team to build
+                          devops site features, automation framework features,
+                          and CLI tools<br></br>
+                          •Researched, pitched, and succesfully developed an
+                          automated cross-build failure analysis feature for the
+                          devops website that saved the development team over
+                          100 hours/year during automated run analysis
+                          <br></br>•Worked on the fullstack development of the
+                          feature; wrote optimized SQL queries and string
+                          concatenation algorithms
+                          <br></br>•Developed the frontend dashboard template
+                          and utilized python RegEx filters to clean and
+                          summarize failure description data
+                          <br></br>•Worked on the development of a new python
+                          test automation frameworkthatdecreased automated
+                          regression run times by 50% to speed up the deployment
+                          of the Exstream software suite; Implemented OOP
+                          modular design, framework functions todecrease
+                          generated file sizes, and decreased VM resource usage
+                          <br></br>•Developed, troubleshooted and fixed new and
+                          existing framework features to improve VM resource
+                          management
+                          <br></br>•Wrote 100+ new python test automation
+                          scripts for build/database coverage during automated
+                          regression testing
+                          <br></br>•Used {""}
+                          <strong>
+                            Python, CherryPy, SQL Alchemy, Mako, SVN{" "}
+                          </strong>
+                        </p>
                       </div>
                     </div>
                   </article>
-                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Software Developer - High Level Data Fusion <br></br>
+                          <span>Waterloo, Ontario </span>
+                          <br></br>
+                          <span>Jan. 2018 - May. 2019</span>
+                        </h2>
+                        <h2>WATonomous</h2>
+                        <p>
+                          •Worked in an autonomous vehicle data processing team
+                          that developed the data fusion pipeline for matching
+                          lidar, radar, and camera data; used ROS kinetic
+                          (client-server protocol framework), Ubuntu 16.04,
+                          vagrant virtual machine, and C++<br></br>
+                          •Developed a data publishing microservice to match
+                          lidar and camera data; implemented time
+                          synchronization between different incoming frequencies
+                          of lidar and camera data to reduce fusion error
+                          <br></br>•Worked on a depth association algorithm to
+                          associate lidar and camera data; developed an object
+                          detection matching function based off of the
+                          intersection of unions evaluation metric which passed
+                          the KITTI dataset unit tests with 99% accuracy
+                          <br></br>•Researched & developed lane matching and
+                          threshold function algorithms using dynamic time
+                          warping optimization method
+                          <br></br>•Used {""}
+                          <strong>C++, ROS, Python, Ubuntu 16.04, Git</strong>
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-2">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Automation Design Engineering Intern <br></br>
+                          <span>Guelph, Ontario </span>
+                          <br></br>
+                          <span>May. 2018 - Aug. 2018</span>
+                        </h2>
+                        <h2>Linamar Gear</h2>
+                        <p>
+                          •Worked in the linamar gear engineering team on
+                          automation projects to help increase manufacturing
+                          throughput by 30%; designed robot end of arm tooling,
+                          orientation systems, and conveyor equipment on new
+                          production lines for Ford and GM vehicles<br></br>
+                          •Led the research & development of 3d printed
+                          prototypes for new automated gear orientation systems
+                          to reduce fixture costs
+                          <br></br>•Used {""}
+                          <strong>SolidWorks, AutoCAD, FEA</strong>
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
                     <div className="timeline-entry-inner">
                       <div className="timeline-icon color-5">
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Primary and Higher Education <span>2003-2015</span></h2>
-                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths with 91 merit. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+                        <h2>
+                          Contract Drone Design <br></br>
+                          <span>Guelph, Ontario </span>
+                          <br></br>
+                          <span>May. 2018 - Aug. 2018</span>
+                        </h2>
+                        <h2>Private Drone Contractor</h2>
+                        <p>
+                          •Designed Drone parts using Solidworks and Inventor
+                          Software<br></br>
+                          •Designed Drone shell and Landing Gear Supporters;
+                          utilized advanced surface modelling for optimized
+                          Carbon Fibre Reinforced Polymer Strengthening process
+                          <br></br>
+                          •Conducted FEA and CFD simulations on drone shell to
+                          ensure aerodynamic design as well as lightweight but
+                          supported frame structure
+                          <br></br>•Used {""}
+                          <strong>SolidWorks, AutoCAD, FEA, CFD</strong>
+                        </p>
                       </div>
                     </div>
                   </article>
-                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
                     <div className="timeline-entry-inner">
-                      <div className="timeline-icon color-none">
+                      <div className="timeline-icon color-1">
+                        <i className="icon-pen2" />
                       </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Process Engineering Intern <br></br>
+                          <span>London, Ontario </span>
+                          <br></br>
+                          <span>Sept. 2017 - Dec. 2017 </span>
+                        </h2>
+                        <h2>TSC Stores, Corporate Office</h2>
+                        <p>
+                          •In this role as a process engineering intern, I
+                          spearheaded multiple lean continous improvement
+                          projects throughout their development cycle for the
+                          distribution center. For instance, I worked on
+                          developing new lean product picking, recieving, and
+                          storage processes with focuses on decreasing cycle
+                          time and decreasing process costs<br></br>
+                          •Managed lean process improvement projects using A3,
+                          Process Maps, VSM, RCA, time studies and DOE to
+                          measure, record, analyze and validate project
+                          initiatives; commended by corporate president for
+                          excellence in A3 project development
+                          <br></br>•Managed the development of a $2 million
+                          automated sortation system to increase product sorting
+                          efficiency / used AutoCAD to produce conceptual
+                          drawings and integration schematics
+                          <br></br>•Led product flow optimization project
+                          through planning and implementation phases to increase
+                          process efficiency by 30% and save $200K in labor
+                          spending’s annually
+                          <br></br>•Used {""}
+                          <strong>AutoCAD, Excel</strong>
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+
+                  <article
+                    className="timeline-entry begin animate-box"
+                    data-animate-effect="fadeInBottom"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none"></div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-1">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Wireless Systems Software Research Engineering Intern{" "}
+                          <br></br>
+                          <span>Waterloo, Ontario </span>
+                          <br></br>
+                          <span>Jan. 2017 - Apr. 2017</span>
+                        </h2>
+                        <h2>University of Waterloo</h2>
+                        <p>
+                          •Worked in a wireless systems research lab developing
+                          novel IOT projects to detect motion and material
+                          composition<br></br>
+                          •Worked on using bluetooth low energy for automatic
+                          device pairing; modified motion interrupts and package
+                          advertising functions to add motion data to bluetooth
+                          packets
+                          <br></br>•Developed a novel occupancy detection system
+                          using bluetooth low energy transmitters and receivers;
+                          co-authored an extensive research paper based on the
+                          new detection system to be published in the IEEE
+                          journal
+                          <br></br>•Developed, tested and optimized the room
+                          occupancy detection system; worked on a MATLAB script
+                          using fourier transforms and other signal processing
+                          methods to provide real-time detection feedback during
+                          development
+                          <br></br>•Used {""}
+                          <strong>C, Python, MATLAB, Git</strong>
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+
+                  <article
+                    className="timeline-entry begin animate-box"
+                    data-animate-effect="fadeInBottom"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none"></div>
                     </div>
                   </article>
                 </div>
@@ -60,6 +283,6 @@ export default class Timeline extends Component {
           </div>
         </section>
       </div>
-    )
+    );
   }
 }
